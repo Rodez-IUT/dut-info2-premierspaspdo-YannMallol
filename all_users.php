49aa5e5aaaabaa77b366fc46e1252a9a05ffb821
yann.mallol <yann.mallol@iut-rodez.fr>
2019-11-13 12:14:03 +0100

    fix #2 Récupération et affichage des utilisateurs avec énoncé non préparé

--- all_users.php
+++ all_users.php
@@ -17,18 +17,19 @@
     <body>
 		<table>
 			<tr> 
-				<td> ID </td>
-				<td> Username </td>
-				<td> Email </td>
-				<td> Status </td>
+				<th> ID </th>
+				<th> Username </th>
+				<th> Email </th>
+				<th> Status </th>
 			</tr>
 			<?php 
 				$host = 'localhost';
-				$db   = 'my-activities';
+				$port = "3306";
+				$db   = 'my_activities';
 				$user = 'root';
 				$pass = 'root';
 				$charset = 'utf8mb4';
-				$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+				$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
 				$options = [
 					PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
 					PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
@@ -39,17 +40,17 @@
 					$pdo = new PDO($dsn, $user, $pass, $options);
 				} catch (PDOException $e) {
 					throw new PDOException($e->getMessage(), (int)$e->getCode());
-				}
-				$stmt = $pdo->query('SELECT email FROM users'); //TODO faire la bonne requêtre pour chaque tr/td
+				} 
+				
+				$stmt = $pdo->query('SELECT * FROM users JOIN status ON status_id = status.id ORDER BY username'); 
+				
 				while ($row = $stmt->fetch())
 				{
-					echo $row['email'] . "\n";
+					echo "<tr>"."<td>".$row['id'] ."</td><td>".$row['username']."</td><td>".$row['email']."</td>";
+					echo "<td>".$row['name']."</td>"."</tr>";
 				}
-				// <tr> 
-					// <td> ID </td>
-					// <td> Username </td>
-					// <td> Email </td>					<td> Status </td>
-				// </tr>
+				
+				   
 			?>
 		</table>
 	  

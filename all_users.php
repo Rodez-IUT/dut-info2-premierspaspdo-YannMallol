1445270180ba9eed6004a416985dee4087fa90a6
yann.mallol <yann.mallol@iut-rodez.fr>
2019-11-27 12:16:46 +0100

    5.2

--- all_users.php
+++ all_users.php
@@ -1,83 +1,89 @@
 <!DOCTYPE html>
-<html lang="fr">
-  <head>
-		<meta charset="utf-8">
-		<title>Activité 2</title>
-		<!-- Lien vers mon CSS -->
-		<!--<link href="css/monStyle.css" rel="stylesheet"> -->
-		
-		<!-- Bootstrap CSS -->
-		<link href="bootstrap/css/bootstrap.css" rel="stylesheet">
-		
-		<!-- Police Font Awesome pour les icônes -->
-		<link href="fontawesome-free-5.10.2-web/css/all.css" rel="stylesheet">
-		
-   </head>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>All users</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+        th, td {
+            padding: 8px;
+            text-align: left;
+            border-bottom: 1px solid #ddd;
+        }
+    </style>
+</head>
+<body>
 
-    <body>
-		<table>
-			<tr> 
-				<th> ID </th>
-				<th> Username </th>
-				<th> Email </th>
-				<th> Status </th>
-			</tr>
-			
-			
-			<?php 
-				$status_id = '%';
-				$lettre = '';
-				$ok = false;
-				$host = 'localhost';
-				$port = "3306";
-				$db   = 'my_activities';
-				$user = 'root';
-				$pass = 'root';
-				$charset = 'utf8mb4';
-				$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
-				$options = [
-					PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-					PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-					PDO::ATTR_EMULATE_PREPARES   => false,
-					];
-				
-				
-				try {
-					$pdo = new PDO($dsn, $user, $pass, $options);
-				} catch (PDOException $e) {
-					throw new PDOException($e->getMessage(), (int)$e->getCode());
-				} 
-				
-				if(isset($_GET["lettre"]) && isset($_GET["status"])){
-					$status_id = $_GET["status"];
-					$lettre = $_GET["lettre"];
-					$ok = true;
-				}
-			?>
-			<form method="get" action="all_users.php">
-				<fieldset>
-					<p> Start with letter <input type="text" name="lettre" id="lettre" <?php if($ok == true) {echo 'value="'.$lettre.'"';}?>>  
-						<select name="status" id="status">
-							<option> Active account
-							<option> Waiting for account validation 	
-						</select>
-						<input type="submit"></input>
-					</p>
-				</fieldset>
-			</form>
-			<?php
-				//$stmt = $pdo->query('SELECT * FROM users JOIN status ON status_id = status.id ORDER BY username'); 
-				$stmt = $pdo->query('SELECT * FROM users JOIN status ON status_id = status.id  WHERE name = \''.$status_id.'\' AND username LIKE \''.$lettre.'%\'ORDER BY username'); 
-				while ($row = $stmt->fetch())
-				{
-					echo "<tr>"."<td>".$row['id'] ."</td><td>".$row['username']."</td><td>".$row['email']."</td>";
-					echo "<td>".$row['name']."</td>"."</tr>";
-				}
-				
-				
-				   
-			?>
-		</table>
-	  
-	</body>
+<?php
+$host = 'localhost';
+$port = '3306';
+$db = 'my_activities';
+$user = 'root';
+$pass = 'root';
+$charset = 'utf8mb4';
+$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+    throw new PDOException($e->getMessage(), (int)$e->getCode());
+}
+function get($name) {
+    return isset($_GET[$name]) ? $_GET[$name] : null;
+}
+?>
+
+<h1>All Users</h1>
+
+<form action="all_users.php" method="get">
+    Start with letter:
+    <input name="start_letter" type="text" value="<?php echo get("start_letter") ?>">
+    and status is:
+    <select name="status_id">
+        <option value="1" <?php if (get("status_id") == 1) echo 'selected' ?>>Waiting for account validation</option>
+        <option value="2" <?php if (get("status_id") == 2) echo 'selected' ?>>Active account</option>
+		<option value="3" <?php if (get("status_id") == 3) echo 'selected' ?>>Waiting for account deletion</option>
+		
+    </select>
+    <input type="submit" value="OK">
+</form>
+
+<?php
+$action = "askDeletion";
+$start_letter = htmlspecialchars(get("start_letter").'%');
+$status_id = (int)get("status_id");
+$sql = "select users.id as user_id, username, email, s.name as status from users join status s on users.status_id = s.id where username like :start_letter and status_id = :status_id order by username";
+$stmt = $pdo->prepare($sql);
+$stmt->execute(['start_letter' => $start_letter, 'status_id' => $status_id]);
+?>
+<table>
+    <tr>
+        <th>Id</th>
+        <th>Username</th>
+        <th>Email</th>
+        <th>Status</th>
+    </tr>
+    <?php while ($row = $stmt->fetch()) { ?>
+        <tr>
+            <td><?php echo $row['user_id'] ?></td>
+            <td><?php echo $row['username'] ?></td>
+            <td><?php echo $row['email'] ?></td>
+            <td><?php echo $row['status'] ?></td>
+			<?php if (get("status_id") != 3) {?>
+			<td><a href="all_users.php?status_id=3&user_id=<?php get("user_id")?>&action=askDeletion">askDeletion</a></td>
+			<?php }?>
+        </tr>
+    <?php } ?>
+</table>
+
+
+</body>
 </html>
